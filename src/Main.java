852ba024dfb4d1b3ad1026c6df7dcdacc36610ce
Alparslanygd <alparslanyeniden@gmail.com>
2022-11-06 18:14:59 +0300

    Write a program that displays the result of; 9.5 X 4.5 - 2.5 X 3 / 45.5 - 3.5

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Write a program that displays the result of; 9.5 X 4.5 - 2.5 X 3 / 45.5 - 3.5
+
+        System.out.println("Resut: " +((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
